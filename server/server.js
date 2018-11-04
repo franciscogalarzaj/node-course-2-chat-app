449478d09d44cfc84626a83275f626af6f3c5f68
Francisco <franciscogalarzaj@gmail.com>
2018-11-04 02:56:08 -0300

    Add geolocation support via geolocation api and test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -20,11 +20,15 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'Great someone else has joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log(message);      
         io.emit('newMessage', generateMessage(message.from, message.text));  
-
+        callback();
     });
+
+    socket.on('createLocationMessage', (coords) => {        
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+      });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');            
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage',() => {
     it('should return message object', () => {
@@ -10,6 +10,19 @@ describe('generateMessage',() => {
 
         expect(message.from).toBe(testFrom);
         expect(message.text).toBe(testText);
+        expect(message.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should return locationMessage object', () => {
+        var testFrom = 'Admin';
+        var latitude = 1;
+        var longitude = 1;
+        var message = generateLocationMessage(testFrom, latitude, longitude);
+
+        expect(message.from).toBe(testFrom);
+        expect(message.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
         expect(message.createdAt).toBeA('number');
     });
 });
